15d096bc91efd4c0c91086f52be5b25f0f95b324
Brian Ridolce <brianridolce@gmail.com>
2020-03-26 22:07:09 +0100

    création de seeds simples

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,23 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+  validates :start_date, :end_date, presence: true
+  # validate :date_start_after_today?
+  # validate :end_date_after_start_date?
+
+  private
+
+  def date_start_after_today?
+    return if start_date.blank?
+
+    errors.add(:start_date, "must be equal or after today") if start_date < Date.today
+  end
+
+  def end_date_after_start_date?
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "must be after the start date") if end_date < start_date
+  end
+
 end
