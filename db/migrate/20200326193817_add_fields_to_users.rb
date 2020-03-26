3d5daf4dcce02a294aa76357ab08648a0a17d241
Paul Portier <paulportier@gmail.com>
2020-03-26 20:44:19 +0100

    Users instantiated with roles and attributes

--- db/migrate/20200326193817_add_fields_to_users.rb
+++ db/migrate/20200326193817_add_fields_to_users.rb
@@ -3,5 +3,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :last_name, :string
     add_column :users, :first_name, :string
     add_column :users, :phone, :string
+    add_column :users, :role, :integer
   end
 end
