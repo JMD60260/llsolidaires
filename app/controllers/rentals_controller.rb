f162c10105e2b376c17cf3237845d5deceef552e
Paul Portier <paulportier@gmail.com>
2020-03-26 22:44:30 +0100

    basic stuff

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,41 @@
 class RentalsController < ApplicationController
+  before_action :set_rental, only: [:show, :edit, :update, :destroy]
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_rental
+    @rental = Rental.find(params[:id])
+  end
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date, :limit_date, :user_id, :flat_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   enum role: [:owner, :medical]
 
+  after_initialize do
+    if self.new_record?
+      self.role ||= :medical
+    end
+  end
+
+  validates :role, :phone, :last_name, presence: true
 end
