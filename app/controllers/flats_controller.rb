00f5e340e78902a5feaab14ab11c46c1f2e7fe9b
Brian Ridolce <brianridolce@gmail.com>
2020-03-27 11:24:41 +0100

    initialisation du Geocoder + Initialisation View Index FLats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,11 +2,18 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def show
-
+    @flat = Flat.find(params[:id])
   end
 
   def index
+     @flats = Flat.geocoded
 
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def new
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,4 +3,8 @@ class Flat < ApplicationRecord
   has_many :rentals
 
   validates :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
 end
