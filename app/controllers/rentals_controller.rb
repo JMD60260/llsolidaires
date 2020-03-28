0c916c082d1313da822eb73637174b4bec5c7711
youenn priser <youenn.priser@gmail.com>
2020-03-28 20:08:17 +0100

    Began to implement index, nex, create actions

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,14 +6,37 @@ class RentalsController < ApplicationController
   end
 
   def index
-
+    if current_user.role == 'owner'
+      # Find the current user flats
+      @flats = Flat.where(user: current_user)
+      # Instanciate an empty array of rentals record
+      @rentals = []
+      # Iterate over current user 's flats to find the corresponding rentals
+      @flats.each do |flat|
+        # Store each flat's rentals
+        flat_rentals = Rental.where(flat: flat)
+        # Iterate over these to store it in the @rentals variable
+        flat_rentals.each do |rental|
+          @rentals << rental
+        end
+      end
+    else # elsif current_user.role == 'owner'
+      @rentals = Rental.where(user: current_user)
+    end
   end
 
   def new
-
+    # To access in a partial
+    @rental = Rental.new
   end
 
   def create
+    if current_user == 'medical'
+      @rental = Rental.new(rental_params)
+    else
+      flash[:error] = "Accès interdit -> Vous devez vous inscrire d'abord !"
+      redirect_to root_path
+    end
 
   end
 
