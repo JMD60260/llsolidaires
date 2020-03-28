7fd81727ca84bbab8a0a6a1561c9a9a425f2cf62
youenn priser <youenn.priser@gmail.com>
2020-03-28 18:40:14 +0100

    Validate no error on db

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_28_120439) do
+ActiveRecord::Schema.define(version: 2020_03_28_121605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_120439) do
     t.float "latitude"
     t.float "longitude"
     t.string "status", default: "libre"
+    t.json "photos"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
