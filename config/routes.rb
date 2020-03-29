4dbffa31348f6ae9e8e72c4bf25e45e4c9a8093c
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-29 17:23:55 -0400

    Rentals controller prêt avec ses views

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,19 @@ Rails.application.routes.draw do
   get 'dashboard/owner', to: "dashboards#owner"
   get 'dashboard/medical', to: "dashboards#medical"
 
-  resources :flats do
-    resources :rentals, only: [:create]
+  resources :flats, only: [:create, :edit, :update, :destroy]
+
+  resources :rentals, only: [:create, :destroy] do
+    member { get :download_proof }
+    patch 'validate', to: "rentals#validate_rental"
+    delete 'refuse', to: "rentals#refuse_rental"
   end
 
-  resources :rentals, only: [:show, :index, :edit] do
-    member { get :download_proof }
-  end
+  get 'owner-validated-rentals', to: "rentals#owner_validated"
+  get 'owner-pending-requests', to: "rentals#owner_pending"
+
+  get 'medical-validated-rentals', to: "rentals#medical_validated"
+  get 'medical-pending-requests', to: "rentals#medical_pending"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
