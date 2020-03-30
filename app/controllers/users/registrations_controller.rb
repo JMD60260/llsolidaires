d228f84ea7d098e30bea17f3ac59de92dfeba913
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-30 19:03:25 -0400

    Reste à ajouter le style/l'html sur la page sessions/new pour le nb d'apparts par ville

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    @cities = Flat.pluck(:city).uniq
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   # def create
