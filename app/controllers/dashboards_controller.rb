1f44e5e94934c5c265a8b8d934ca182e25470490
Julie <maslorzj@yahoo.fr>
2020-03-31 01:16:45 +0200

    fix map markers and searchbox marker focus

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
     @flat = Flat.new
   end
 
-def medical
+  def medical
      @flats = Flat.where("address ILIKE ?", "%#{params[:query]}%")
      if params[:query]
 
@@ -18,25 +18,23 @@ def medical
 
       puts "je suis dans le if"
       @flats = Flat.near(params[:query], 20)
-      @markers = @flats.map do |flat|
-        {
-          lat: flat.geocode[0],
-          lng: flat.geocode[1],
-          # infoWindow: { content: render_to_string(partial: "/flats/maps", locals: { flat: flat }) }
-        }
-      end
     else
       puts "je suis dans le else"
       @flats = Flat.all
-      @markers = @flats.map do |flat|
-        {
-          lat: flat.geocode[0],
-          lng: flat.geocode[1],
-          # infoWindow: { content: render_to_string(partial: "/flats/maps", locals: { flat: flat }) }
-        }
-      end
     end
+    @markers = geocoded_flats
   end
     # raise
 
+  private
+
+  def geocoded_flats
+    @flats.map do |flat|
+      {
+        lat: flat.geocode[0],
+        lng: flat.geocode[1],
+        # infoWindow: { content: render_to_string(partial: "/flats/maps", locals: { flat: flat }) }
+      }
+    end
+  end
 end
