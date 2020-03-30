ff72fdc09de4ca1956ddae040d77144d2299a39a
Ines Belghiti <ines.belghiti@gmail.com>
2020-03-30 13:41:59 -0400

    Logiue ajoutée via les controlleurs de devise, reste style

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,10 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    @cities = Flat.pluck(:city).uniq
+    super
+  end
 
   # POST /resource/sign_in
   # def create
